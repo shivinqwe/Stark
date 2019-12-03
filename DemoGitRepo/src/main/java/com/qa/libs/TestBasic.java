59669618d8fe49232eb7dfcabb3be826a3f715d4
Kranthi <Kranthi@Shiva>
2019-12-03 18:02:33 +0530

    commit it

---
+++ DemoGitRepo/src/main/java/com/qa/libs/TestBasic.java
@@ -0,0 +1,11 @@
+package com.qa.libs;
+
+public class TestBasic {
+
+
+		public void test() {
+			System.out.println("test123");
+
+	}
+
+}
---
+++ DemoGitRepo/src/main/java/com/qa/pages/LoginPage.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class LoginPage {
+	
+	public void loginmethod() {
+		System.out.println("login method");
+	}
+
+}
